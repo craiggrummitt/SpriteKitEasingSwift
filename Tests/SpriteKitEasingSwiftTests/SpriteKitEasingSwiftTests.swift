c311d06daa5faec2560d3b6e87f44a1ce9e93c27
Dmitry Shlepkin <acidl33t@gmail.com>
2024-11-11 20:35:53 -0500

    Remove CocoaPod support files, Update Example App With local module, Move tests from example app to package it self

--- Tests/SpriteKitEasingSwiftTests/SpriteKitEasingSwiftTests.swift
+++ Tests/SpriteKitEasingSwiftTests/SpriteKitEasingSwiftTests.swift
@@ -6,13 +6,33 @@
 //
 
 import XCTest
+import SpriteKit
 @testable import SpriteKitEasingSwift
 
 final class SpriteKitEasingSwiftTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual("Hello, World!", "Hello, World!")
+    
+    func testEasing() throws {
+        var easeMe: CGFloat = 0
+        let easingFunction = SKEase.getEaseFunction(.curveTypeLinear, easeType: .easeTypeIn)
+        let action = SKEase.createFloatTween(0, end: 1, time: 0.3, easingFunction: easingFunction) { (node: SKNode, easeValue: CGFloat) -> Void in
+            easeMe = easeValue
+        }
+    
+        let scene = SKScene()
+        let view = SKView()
+        let node = SKNode()
+        
+        view.presentScene(scene)
+        scene.addChild(node)
+        node.run(action)
+    
+        let exp = expectation(description: "Test after 3 second")
+        let result = XCTWaiter.wait(for: [exp], timeout: 3.0)
+        if result == XCTWaiter.Result.timedOut {
+            XCTAssertTrue(easeMe == 1.0)
+        } else {
+            XCTFail("Delay interrupted")
+        }
     }
+    
 }
